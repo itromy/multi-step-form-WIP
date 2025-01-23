130cf3e1e5485254ae7160aadb0b93b2c66db736
Romy Pohl <romy.pohl.rp@gmail.com>
2025-01-23 18:19:59 +0100

    - squash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Link from "next/link";
-
 export const metadata: Metadata = {
   title: "Multi Step Form",
   description: "Coding Challenge by Frontend Mentor",
@@ -16,14 +14,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <footer>
-          <ul>
-              <li><Link href="/your-info">Step 1</Link></li>
-              <li><Link href="/add-ons">Step 2</Link></li>
-              <li><Link href="/select-plan">Step 3</Link></li>
-              <li><Link href="/summary">Step 4</Link></li>
-          </ul>
-        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <main>
+        <ul>
+              <li><Link href="/your-info">Step 1</Link></li>
+              <li><Link href="/add-ons">Step 2</Link></li>
+              <li><Link href="/select-plan">Step 3</Link></li>
+              <li><Link href="/summary">Step 4</Link></li>
+          </ul>
       </main>
     </div>
   );
