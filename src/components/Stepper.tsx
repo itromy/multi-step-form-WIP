c78475024fa2007e38336ef6bdd6cd6303e466c6
Romy Pohl <romy.pohl.rp@gmail.com>
2025-01-23 20:24:43 +0100

    feat(stepper): add stepper component

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,33 @@
+import styles from './Stepper.module.css';
+
+export type StepperProps = {
+    steps: Array<Step>;
+    active: number;
+}
+
+export type Step = {
+    label: string;
+}
+
+export default function Stepper({steps, active}: StepperProps) {
+    function renderStep(step: Step, index: number) {
+        return (
+            <li className={styles.step}>
+                <span className={`${styles.index} ${(index + 1) === active ? styles.activeIndex:  ''}`}>{index + 1}</span>
+                <span className={styles.sublineLabelWrapper}>
+                    <span className={styles.stepSubline}>Step {index + 1}</span>
+                    <span className={styles.label}>{step.label}</span>
+                </span>
+            </li>
+        )
+    }
+
+    return (
+        <nav>
+            <ul className={styles.list}>
+                {steps.map(renderStep)}
+            </ul>
+        </nav>
+    )
+}
+
---
+++ src/constants/StepperSteps.ts
@@ -0,0 +1,19 @@
+import { Step } from "@/components/Stepper";
+
+const STEPPER_STEPS : Array<Step> = [
+    {
+        label: "Your Info"
+    },
+    {
+        label: "Select Plan"
+    },
+    {
+        label: "Add-Ons"
+    },
+    {
+        label: "Summary"
+    }
+
+];
+
+export default STEPPER_STEPS;
