64a263ae81b985bc9b703d0e7d2e299c1ae241f5
Romy Pohl <romy.pohl.rp@gmail.com>
2025-01-23 18:54:10 +0100

    feat(container): add background color prop

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,11 +3,16 @@ import styles from './Container.module.css';
 
 export type ContainerProps = {
     children: React.ReactNode;
+    backgroundColor?: string;
 }
 
-export default function Container({ children } : ContainerProps) {
+export default function Container({ children, backgroundColor } : ContainerProps) {
+    const style = {
+        backgroundColor,
+    }
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} style={style}>
             {children}
         </div>
     )
