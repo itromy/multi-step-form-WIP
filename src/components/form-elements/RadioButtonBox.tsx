84b5716f925f3a4e4587d1c99a59634999d7adcb
Romy Pohl <romy.pohl.rp@gmail.com>
2025-01-24 14:54:33 +0100

    feat(radio-button-box): edit styles, add on change

--- src/components/form-elements/RadioButtonBox.tsx
+++ src/components/form-elements/RadioButtonBox.tsx
@@ -5,16 +5,24 @@ export type RadioButtonBoxProps = {
     children: React.ReactNode;
     name: string;
     value: string;
+    checked?: boolean;
+    onChange: ({name , value}: {name:string, value: string}) => void;
 }
 
-export default function RadioButtonBox({children, name, value}: RadioButtonBoxProps) {
+export default function RadioButtonBox({children, name, value, checked, onChange}: RadioButtonBoxProps) {
+    function handleOnChange() {
+        onChange({
+            name: name,
+            value: value,
+        })
+    }
+    
     return(
-        <label className={styles.component}>
+        <label className={`${styles.component} ${checked ? styles.checked : ''}`}>
             <span className={styles.child}>
                 {children}
             </span>
-            <input className={`${styles.input} visually-hidden`} type="radio" name={name} value={value}/>
-            <span className={styles.border}/>
-        </label>
+            <input onChange={handleOnChange} className={`${styles.input} visually-hidden`} type="radio" name={name} value={value} checked={checked}/>
+       </label>
     )
 }
